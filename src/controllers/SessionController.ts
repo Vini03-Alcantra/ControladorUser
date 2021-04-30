cd038b2daf45a8afe630eed632f2229e756b3559
Vinicus <viniciusalcantarabarros@gmail.com>
2021-04-29 23:51:45 -0300

    criando session de user

---
+++ src/controllers/SessionController.ts
@@ -0,0 +1,34 @@
+import {Request, Response} from "express";
+import { getCustomRepository } from "typeorm";
+import UserRepository from "../repositories/UserRepository";
+import {compare} from 'bcryptjs';
+import {sign} from "jsonwebtoken";
+
+class SessionController {
+    async create(request: Request, response: Response){
+        const {username, password} = request.body;
+        const userRepository = getCustomRepository(UserRepository)
+        const user = await userRepository.findOne({username})
+
+        if(!user){
+            return response.status(400).json({error: "User not found"})
+        }
+
+        const matchPassword = await compare(password, user.password)
+        if(!matchPassword){
+            return response.status(400).json({error: "Incorrect Password or username"})
+        }
+
+        const token = sign({}, "62988e7abd164a1e1b549f41fc0ae440", {
+            subject: user.id,
+            expiresIn: "1d"
+        })
+
+        return response.json({
+            token,
+            user
+        })
+    }
+}
+
+export default new SessionController;
