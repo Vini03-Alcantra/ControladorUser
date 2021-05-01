63dd8866a079bd0c0e3ebbeadd38ceb88bc1ca0a
Vinicus <viniciusalcantarabarros@gmail.com>
2021-04-30 23:52:13 -0300

    Inserindo em products

--- src/routes.ts
+++ src/routes.ts
@@ -3,11 +3,16 @@ import UserController from "./controllers/UserController";
 import SessionController from "./controllers/SessionController";
 import PermissionController from "./controllers/PermissionController";
 import RoleController from "./controllers/RoleController";
+import ProductController from "./controllers/ProductController";
 const router = Router()
 
 router.post("/users", UserController.create)
 router.post("/sessions", SessionController.create)
 router.post("/permissions", PermissionController.create)
 router.post("/roles", RoleController.create)
+
+router.post("/products", ProductController.create)
+router.post("/products", ProductController.index)
+router.post("/products/:id", ProductController.show)
 
 export {router}
